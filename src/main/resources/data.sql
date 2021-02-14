142e19e4418d4449da6cbc0afa746e45df5138fc
amale <aidrissi001@gmail.com>
2021-02-14 19:49:26 +0100

    add of data.sql file + update of properties file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into user(name, address, city, email, password) values('John Boyd', '1509 Culver St', 'Culver', 'jaboyd@email.com', 'jb1234');
+insert into user(name, address, city, email, password) values('Felicia Carman', '834 Binoc Ave', 'London', 'feliciacarman@email.com', 'felicia_1234');
+insert into user(name, address, city, email, password) values('Jonathan Marrack', '29 15th St', 'Manchester', 'jmarrack@email.com', 'jMarrack_56_78');
+insert into user(name, address, city, email, password) values('Lily Cooper', '489 Manchester St', 'Manchester', 'lily@email.com', 'james_bond_007');
+insert into user(name, address, city, email, password) values('Sophia Zemicks', '892 Downing Ct', 'Liverpool', 'soph@email.com', '892meganMarkel892');
